697a5334ebf3ec64d40537bd9fb7c0d147a85020
chenriqueend <carloshenriqueend@gmail.com>
2022-04-18 17:06:31 -0300

    landing page v1.2

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
               <img src={logo} className="App-logo" alt="logo" />
             </div>
             <div className='info'>
-              <h3>O Passeio de Jupiter é um projeto contemplado pela Lei Aldir Blanc. Neste momento estamos em manutenção (˃ᆺ˂).</h3>
-              <h3>Mais informações no nosso  <a href='https://instagram.com/opasseiodejupiter?igshid=YmMyMTA2M2Y='><SocialIcon network="instagram" /></a></h3>
+              <h3>O Passeio de Jupiter é um projeto contemplado pela Lei Aldir Blanc. Neste momento estamos em manutenção.<br></br>(˃ᆺ˂)</h3><br></br>
+              <h3>Mais informações no nosso:  <a href='https://instagram.com/opasseiodejupiter?igshid=YmMyMTA2M2Y='><SocialIcon network="instagram" /></a></h3>
             </div>
           </div>
         </div>
