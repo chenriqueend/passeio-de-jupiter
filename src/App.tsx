a3cee83bae13e639ca20aa54989e947e6e296187
chenriqueend <carloshenriqueend@gmail.com>
2022-04-18 13:57:50 -0300

    test heroku

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
             </div>
             <div className='info'>
               <h3>O Passeio de Jupiter é um projeto contemplado pela Lei Aldir Blanc. Neste momento estamos em manutenção (˃ᆺ˂).</h3>
-              <h3>Mais informações no nosso  <a href='https://instagram.com/opasseiodejupiter?igshid=YmMyMTA2M2Y='><SocialIcon url="https://jaketrent.com" network="instagram" /></a></h3>
+              <h3>Mais informações no nosso   <a href='https://instagram.com/opasseiodejupiter?igshid=YmMyMTA2M2Y='><SocialIcon url="https://jaketrent.com" network="instagram" /></a></h3>
             </div>
           </div>
         </div>
