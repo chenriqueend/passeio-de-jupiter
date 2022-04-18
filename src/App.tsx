2ec7d4959edb33a8187623e1fd52d07d9e72a3d6
chenriqueend <carloshenriqueend@gmail.com>
2022-04-18 13:51:57 -0300

    landing page v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
+import { SocialIcon } from 'react-social-icons';
+
+
+// React.render(<SocialIcon url="https://linkedin.com/in/jaketrent" />, document.body);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className='center'>
+          <div className='menu'>
+            <div className='logo'>
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div className='info'>
+              <h3>O Passeio de Jupiter é um projeto contemplado pela Lei Aldir Blanc. Neste momento estamos em manutenção (˃ᆺ˂).</h3>
+              <h3>Mais informações no nosso  <a href='https://instagram.com/opasseiodejupiter?igshid=YmMyMTA2M2Y='><SocialIcon url="https://jaketrent.com" network="instagram" /></a></h3>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
